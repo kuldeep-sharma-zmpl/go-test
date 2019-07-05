3b1cc18caf684d231a9724d0e86d8527f54c043f
Kuldeep Sharma <kuldeep.sharma@zomato.com>
2019-07-05 13:07:14 +0530

    Test-Git

---
+++ gotut.go
@@ -0,0 +1,14 @@
+
+type redis interface {
+    Set(key string, value inteface{}) int
+    Get(param string) int
+}
+
+func MyFunc(r redis) {
+	r.Set(somevalue)
+}
+
+import "goredis"
+type redis struct {
+    
+}
